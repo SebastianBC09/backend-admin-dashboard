c4d2db8a9de24382fbfedc84b0da61b41a1affde
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-18 23:28:58 -0500

    chore(server): integrate properties routes into server configuration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors';
 import authRoutes from "./src/routes/authRoutes.js";
 import typeRoutes from "./src/routes/typeRoutes.js";
+import propertyRoutes from "./src/routes/propertyRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,9 +11,9 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/types", typeRoutes);
+app.use("/api/properties", propertyRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
