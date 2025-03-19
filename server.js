85c56509cedcc908a3171c718d81e2b2436dc962
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-18 23:11:50 -0500

    fix(server): ES Modules imports

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors';
+import authRoutes from "./src/routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -9,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/types", require("./routes/typeRoutes"));
+app.use("/api/auth", authRoutes);
+// app.use("/api/types", require("./routes/typeRoutes"));
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
