6e3575d39e4e8f3a804b2c88ec05f3975f18ecb5
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-18 23:23:50 -0500

    feat(types): add routes for creating and retrieving types with properties

---
+++ src/routes/typeRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createType, getAllTypes, getTypeById } from "../controllers/typeController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+
+const router = Router();
+
+
+router.get("/", authMiddleware, getAllTypes);
+router.get("/:id", authMiddleware, getTypeById);
+router.post("/", authMiddleware, createType);
+
+export default router;
